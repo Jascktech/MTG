457c846d5fffe698c6a226ccf8317c706d96d965
Jascktech <63076435+Jascktech@users.noreply.github.com>
2022-04-05 19:15:09 -0400

    Add files via upload
    First transfer from Codepen

---
+++ Mtg.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#Creature').change(function() {
+        $('#creature-rules').toggle();
+    });
+});
+$(document).ready(function() {
+    $('#Planeswalker').change(function() {
+        $('#planeswalker-rules').toggle();
+    });
+});
